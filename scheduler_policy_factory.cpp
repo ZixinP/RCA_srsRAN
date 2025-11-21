24317d05f240029fa4056dcc09378ab3b2e40953
unicorn <118615352+ZixinP@users.noreply.github.com>
2025-11-21 17:11:25 +0100

    Add include for greedy scheduler policy
    Added include for scheduler_time_greedy.h to support additional scheduling policy.

--- scheduler_policy_factory.cpp
+++ scheduler_policy_factory.cpp
@@ -1,4 +1,3 @@
-//... inside scheduler_policy_factory.cpp
 #include "scheduler_time_pf.h"
 #include "scheduler_time_rr.h"
 #include "scheduler_time_greedy.h" // <-- ADD THIS INCLUDE
@@ -10,7 +9,6 @@ namespace mac {
 
 std::unique_ptr<scheduler_policy> scheduler_policy_factory::create(const policy_sched_cfg_t& cfg)
 {
-  // Logic for PF scheduler
   if (cfg.pf_sched) {
     SRSRAN_LOG_INFO(logger, "Creating Proportional Fair scheduler policy.");
     return std::make_unique<scheduler_time_pf>(*cfg.pf_sched, logger);
